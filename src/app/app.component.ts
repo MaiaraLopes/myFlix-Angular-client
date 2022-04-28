5b95d7bd9dd1a50af01c563b26b47ce2dfb06c66
MaiaraLopes <73555171+MaiaraLopes@users.noreply.github.com>
2022-04-28 18:56:20 +0100

    Fixed typos and added functionality to Login button on app.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
+import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 
 @Component({
   selector: 'app-root',
@@ -14,6 +15,13 @@ export class AppComponent {
   //Open the dialog when sign up button is clicked
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
+      width: '280px'
+    });
+  }
+
+  //Open the dialog when login button is clicked
+  openUserLoginDialog(): void {
+    this.dialog.open(UserLoginFormComponent, {
       width: '280px'
     });
   }
