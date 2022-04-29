7332c91c2465a992917c660ac416fd82912259d8
MaiaraLopes <73555171+MaiaraLopes@users.noreply.github.com>
2022-04-29 19:05:34 +0100

    Finalized director view in movie-card

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DirectorViewComponent } from '../director-view/director-view.component';
 
 @Component({
   selector: 'app-movie-card',
@@ -9,10 +12,13 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 export class MovieCardComponent implements OnInit {
   movies: any[] = [];
 
-  constructor(public fetchApiData: FetchApiDataService) { }
+  constructor(public fetchApiData: FetchApiDataService,
+    public dialog: MatDialog,
+  public snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.getMovies();
+
   }
 
   getMovies(): void {
@@ -21,5 +27,16 @@ export class MovieCardComponent implements OnInit {
       console.log(this.movies);
       return this.movies;
     });
+  }
+
+  getDirectorView(name: string, bio: string, birth: Date): void {
+    this.dialog.open(DirectorViewComponent, {
+      data: {
+        Name: name,
+        Bio: bio,
+        Birth: birth
+      },
+      width: '500px'
+    })
   }
 }
