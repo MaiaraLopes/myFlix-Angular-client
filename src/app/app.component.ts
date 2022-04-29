1be0b03490c5bd83e6aced5172aa93fbc100de1a
MaiaraLopes <73555171+MaiaraLopes@users.noreply.github.com>
2022-04-29 17:08:01 +0100

    Created welcome-page component and moved code from app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
+
 import { MovieCardComponent } from './movie-card/movie-card.component';
 
 @Component({
@@ -13,19 +11,7 @@ export class AppComponent {
   title = 'myFlix-Angular-client';
 
   constructor(public dialog: MatDialog) { }
-  //Open the registration dialog when sign up button is clicked
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      width: '280px'
-    });
-  }
-
-  //Open the login dialog when login button is clicked
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginFormComponent, {
-      width: '280px'
-    });
-  }
+  
 
   //Open the list of movies when All Movies button is clicked
   openMoviesDialog(): void {
